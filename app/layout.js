c2f564f1820aaa307a5677386b362dec516be7a5
scott-gr <mscottgriffin@gmail.com>
2023-12-20 15:43:23 -0500

    metadata moved to root layout. Base url set to fix imaget

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,24 @@ import './globals.css'
 import './normalize.css'
 import Header from './components/header/Header';
 
+export const metadata = {
+  metadataBase: new URL('https://raidmaps.gg'),
+  title: {
+    template: '%s | raidmaps.gg',
+    default: 'raidmaps.gg',
+  },
+  description: 'Simple raid maps for Destiny 2. Easy to share with your LFG fireteam.',
+  author: 'Fargabarga',
+  openGraph: {
+    title: 'raidmaps.gg',
+    description: 'Simple raid maps for Destiny 2. Easy to share with your LFG fireteam.',
+    type: 'website',
+    url: 'https://raidmaps.gg',
+    siteName: 'raidmaps.gg',
+    locale: 'en_US',
+  },
+};
+
 export default function RootLayout({ children}) {
 
   return (
