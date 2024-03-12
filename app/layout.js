84e9edc4ef840c31be9b7cecc5700ae21e23e214
scott-gr <mscottgriffin@gmail.com>
2024-03-12 10:17:48 -0400

    ir yut revision and crota

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,38 @@
-import './globals.css'
-import './normalize.css'
+import './globals.css';
+import './normalize.css';
 import Header from './components/header/Header';
 
 export const metadata = {
-  metadataBase: new URL('https://raidmaps.gg'),
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/en-US',
-    },
-  },
-  title: {
-    template: '%s | raidmaps.gg',
-    default: 'raidmaps.gg',
-  },
-  description: 'Simple raid maps for Destiny 2. Easy to share with your LFG fireteam.',
-  author: 'Fargabarga',
-  openGraph: {
-    title: 'raidmaps.gg',
-    description: 'Simple raid maps for Destiny 2. Easy to share with your LFG fireteam.',
-    type: 'website',
-    url: 'https://raidmaps.gg',
-    siteName: 'raidmaps.gg',
-    locale: 'en_US',
-  },
+	metadataBase: new URL('https://raidmaps.gg'),
+	alternates: {
+		canonical: '/',
+		languages: {
+			'en-US': '/en-US'
+		}
+	},
+	title: {
+		template: '%s | raidmaps.gg',
+		default: 'raidmaps.gg'
+	},
+	description: 'Simple raid maps for Destiny 2. Easy to share with your LFG fireteam.',
+	author: 'Fargabarga',
+	openGraph: {
+		title: 'raidmaps.gg',
+		description: 'Simple raid maps for Destiny 2. Easy to share with your LFG fireteam.',
+		type: 'website',
+		url: 'https://raidmaps.gg',
+		siteName: 'raidmaps.gg',
+		locale: 'en_US'
+	}
 };
 
-export default function RootLayout({ children}) {
-
-  return (
-    <html lang="en">
-      <body>
-        <Header/>
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			 <body>
+				<Header />
+			    	{children}
+			</body>
+		</html>
+	);
 }
