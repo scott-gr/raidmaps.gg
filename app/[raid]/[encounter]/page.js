d653fdb7fc3d173faaaeaea41658de865817eb56
scott-gr <mscottgriffin@gmail.com>
2024-03-12 13:24:36 -0400

    placeholder text for maps in development. Encounter links for GoS 1&2

--- app/[raid]/[encounter]/page.js
+++ app/[raid]/[encounter]/page.js
@@ -1,15 +1,19 @@
 import raids from '@/app/api/raids.json';
 import Map from '@/app/components/map/Map';
 
-export default function EncounterPage({params}) {
-  const {raid, encounter} = params;
-  const filteredRaid = raids.raids.find(r => r.link === raid);
-  const filteredEncounter = filteredRaid.encounters.find(e => e.link === encounter);
-  const map = filteredEncounter.map;
-  console.log("encounter:" + filteredEncounter)
-  return (
-    <>
-      <Map src={map} alt={filteredEncounter.name}  />
-    </>
-  );
+export default function EncounterPage({ params }) {
+	const { raid, encounter } = params;
+	const filteredRaid = raids.raids.find((r) => r.link === raid);
+	const filteredEncounter = filteredRaid.encounters.find((e) => e.link === encounter);
+	const map = filteredEncounter.map;
+	console.log('encounter:' + filteredEncounter);
+	return (
+		<>
+			{map === '' ? (
+				<div>The {filteredEncounter.name} encounter map is still in development </div>
+			) : (
+				<Map src={map} alt={filteredEncounter.name} />
+			)}
+		</>
+	);
 }
