45a52baf08df05b77e975bbe1433d4bcbfe2b904
scott-gr <mscottgriffin@gmail.com>
2024-04-29 10:20:05 -0400

    removed console log, pantheon-macrocosm map added

--- app/[raid]/[encounter]/page.js
+++ app/[raid]/[encounter]/page.js
@@ -2,18 +2,22 @@ import raids from '@/app/api/raids.json';
 import Map from '@/app/components/map/Map';
 
 export default function EncounterPage({ params }) {
-	const { raid, encounter } = params;
-	const filteredRaid = raids.raids.find((r) => r.link === raid);
-	const filteredEncounter = filteredRaid.encounters.find((e) => e.link === encounter);
-	const map = filteredEncounter.map;
-	console.log('encounter:' + filteredEncounter);
-	return (
-		<>
-			{map === '' ? (
-				<div>The {filteredEncounter.name} encounter map is still in development </div>
-			) : (
-				<Map src={map} alt={filteredEncounter.name} />
-			)}
-		</>
-	);
+  const { raid, encounter } = params;
+  const filteredRaid = raids.raids.find((r) => r.link === raid);
+  const filteredEncounter = filteredRaid.encounters.find(
+    (e) => e.link === encounter
+  );
+  const map = filteredEncounter.map;
+
+  return (
+    <>
+      {map === '' ? (
+        <div>
+          The {filteredEncounter.name} encounter map is still in development{' '}
+        </div>
+      ) : (
+        <Map src={map} alt={filteredEncounter.name} />
+      )}
+    </>
+  );
 }
