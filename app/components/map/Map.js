e20bcfb20c83a3ec0cb38d9a7b6263dffc02c3e6
scott-gr <mscottgriffin@gmail.com>
2024-05-10 10:31:08 -0400

    webp performance test

--- app/components/map/Map.js
+++ app/components/map/Map.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './map.module.css';
+// import careTaker from 'app/api/Caretaker svg final2.svg';
 
 const imgStyle = {
   objectFit: 'contain',
@@ -9,11 +10,12 @@ export default function Map({ src, alt }) {
   return (
     <div className={styles.mapContainer}>
       <Image
-        formats={['image/svg']}
-        priority={true}
+        formats={['image/webp']}
+        priority
         src={src}
         alt={alt}
         fill
+        unoptimized
         style={imgStyle}
       />
     </div>
