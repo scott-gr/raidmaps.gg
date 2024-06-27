a790d4b354e4c86a6c41bed216b05c8aa22dfb86
scott-gr <mscottgriffin@gmail.com>
2024-06-27 15:10:32 -0400

    raid and encounter metadata test

--- app/[raid]/(raid)/page.js
+++ app/[raid]/(raid)/page.js
@@ -5,18 +5,35 @@ export function lowerCase(string) {
   return string.toLowerCase();
 }
 
-export default function RaidPage({params}) {
+export async function generateMetadata({ params }) {
+  const { raid } = params;
+  const filteredRaid = raids.raids.find((r) => r.link === raid);
 
-  const {raid} = params;
-  const filteredRaid = raids.raids.find(r => r.link === raid);
+  return {
+    title: filteredRaid.name + ' maps',
+    description:
+      'Simple, shareable maps for the ' + filteredRaid.name + ' raid',
+    openGraph: {
+      title: filteredRaid.name + ' maps',
+      description:
+        'Simple, shareable maps for the ' + filteredRaid.name + ' raid',
+    },
+  };
+}
 
-  const encounterList = filteredRaid.encounters.map(encounter => {
-    return <Card key={encounter.id} text={encounter.name} link={raid + '/' + encounter.link}></Card>;
+export default function RaidPage({ params }) {
+  const { raid } = params;
+  const filteredRaid = raids.raids.find((r) => r.link === raid);
+
+  const encounterList = filteredRaid.encounters.map((encounter) => {
+    return (
+      <Card
+        key={encounter.id}
+        text={encounter.name}
+        link={raid + '/' + encounter.link}
+      ></Card>
+    );
   });
 
-  return (
-    <>
-      {encounterList}
-    </>
-  );
+  return <>{encounterList}</>;
 }
--- app/[raid]/[encounter]/page.js
+++ app/[raid]/[encounter]/page.js
@@ -1,6 +1,29 @@
 import raids from '@/app/api/raids.json';
 import Map from '@/app/components/map/Map';
 
+export async function generateMetadata({ params }) {
+  const { raid, encounter } = params;
+  const filteredRaid = raids.raids.find((r) => r.link === raid);
+  const filteredEncounter = filteredRaid.encounters.find(
+    (e) => e.link === encounter
+  );
+  return {
+    title: filteredEncounter.name + ' map',
+    description:
+      'Shareable map of the ' +
+      filteredEncounter.name +
+      ' encounter from ' +
+      filteredRaid.name,
+    openGraph: {
+      title: filteredEncounter.name + ' map',
+      description:
+        'Shareable map of the ' +
+        filteredEncounter.name +
+        ' encounter from ' +
+        filteredRaid.name,
+    },
+  };
+}
 export default function EncounterPage({ params }) {
   const { raid, encounter } = params;
   const filteredRaid = raids.raids.find((r) => r.link === raid);
